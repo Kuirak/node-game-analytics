dedca464ee8a86d5ba8283395c64f5f21e9933b7
Kuirak <newdrako@gmx.net>
2014-05-20 18:56:19 +0200

    Refactored editor

---
+++ assets/js/app/directive/node-inputs.js
@@ -0,0 +1,10 @@
+'use strict';
+/**
+ * Created by Jonas Kugelmann on 17.05.2014.
+ */
+app.directive('nodeInputs',function(){
+    return {
+        restrict:'E',
+        templateUrl:'/partials/editor-node-inputs.html'
+    };
+});
---
+++ assets/js/app/directive/node-outputs.js
@@ -0,0 +1,10 @@
+'use strict';
+/**
+ * Created by Jonas Kugelmann on 17.05.2014.
+ */
+app.directive('nodeOutputs',function(){
+   return {
+       restrict:'E',
+       templateUrl:'/partials/editor-node-outputs.html'
+   };
+});
