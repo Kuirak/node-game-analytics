f72953dc1997648f9bc4c4f257e7b0c596238363
Kuirak <newdrako@gmx.net>
2014-05-28 20:04:30 +0200

    Added Auto Display of all internal types  on dashboard

--- assets/js/app/controller/DashboardController.js
+++ assets/js/app/controller/DashboardController.js
@@ -3,46 +3,30 @@
  * Created by Jonas Kugelmann on 27.05.2014.
  */
 app.controller('DashboardController',function($scope,$sailsSocket){
-    $scope.maxScore = 0;
-    $scope.averageScore = 0;
-    $scope.minScore = 0;
-    $scope.roundCount = 0;
+    $scope.data ={} ;
+
 
     (function () {
-       $sailsSocket.subscribe('event',function(message){
-           if(message.verb ==='created'){
-               var event = message.data;
-               if(event.type ==='RoundCount'){
-                   $scope.roundCount = event.params.value;
-               }else if(event.type === 'MaxScore'){
-                   $scope.maxScore = event.params.value;
-               }else if(event.type === 'MinScore'){
-                   $scope.minScore = event.params.value;
-               }else if(event.type === 'AverageScore'){
-                   $scope.averageScore = event.params.value;
-               }
-           }
+        $sailsSocket.subscribe('event',function(message){
+            if(message.verb ==='created'){
+                var event = message.data;
+                if(_.contains(_.keys($scope.data),event.type)){
+                    $scope.data[event.type].data = event.params[_.first(_.keys(event.params))]
+                }
+            }
+        });
+
+       $sailsSocket.get('/api/type?internal=true').success(function(data){
+           _.each(data,function(eventType){
+               $scope.data[eventType.name]={name:eventType.name,data:0};
+               $sailsSocket.get('/api/event?where={"type":"'+eventType.name+'"}&sort=id%20DESC&limit=1').success(function(data){
+                   if(data.length >0) {
+                       $scope.data[data[0].type].data = data[0].params[_.first(_.keys(data[0].params))];
+                   }
+               })
+           })
        });
-        $sailsSocket.get('/api/event?where={"type":"RoundCount"}').success(function(data){
-            if(data.length >0) {
-                $scope.roundCount = _.last(data).params.value;
-            }
-        });
-        $sailsSocket.get('/api/event?where={"type":"MaxScore"}').success(function(data){
-            if(data.length >0) {
-                $scope.maxScore = _.last(data).params.value;
-            }
-        });
-        $sailsSocket.get('/api/event?where={"type":"MinScore"}').success(function(data){
-            if(data.length >0) {
-                $scope.minScore = _.last(data).params.value;
-            }
-        });
-        $sailsSocket.get('/api/event?where={"type":"AverageScore"}').success(function(data){
-            if(data.length >0) {
-                $scope.averageScore = _.last(data).params.value;
-            }
-        });
+
 
     }())
 
